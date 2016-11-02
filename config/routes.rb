92ab9535220683987688d211365c0b2b51b81495
Kurt Thomas <kurt.thomas34@gmail.com>
2016-11-02 17:43:34 +0000

    completed rails testing assignment

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   get 'welcome/about'
   
-  root 'welcome#index'
+  get 'welcome/faq'
+  
+  root 'welcome#faq'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
