0d4b8da3d6424205e67fcd39454e00fdd063b168
Kurt Thomas <kurt.thomas34@gmail.com>
2016-11-10 18:56:00 +0000

    completed assigment 17

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.each_with_index do |post, index|
+      post.title = "Spam" if index % 5 == 0 && index != 0 
+    end
   end
 
   def show
