4a06fa0e27ebd684386953005489c675d0c46f0b
Kurt Thomas <kurt.thomas34@gmail.com>
2016-12-01 20:21:26 +0000

    checkpoint 27 complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161123171024) do
+ActiveRecord::Schema.define(version: 20161130190707) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -30,9 +30,11 @@ ActiveRecord::Schema.define(version: 20161123171024) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
+    t.integer  "user_id"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "sponsored_posts", force: :cascade do |t|
     t.string   "title"
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,22 +9,34 @@
 
 require 'random_data'
 
+#Create Users
+
+5.times do
+  User.create!(
+    name:     RandomData.random_name,
+    email:    RandomData.random_email,
+    password: RandomData.random_sentence
+    )
+end
+users = User.all
+
 # Create Topics
 15.times do
   Topic.create!(
     name: RandomData.random_sentence,
     description: RandomData.random_paragraph
-    )
-  end
-  topics = Topic.all
+  )
+end
+topics = Topic.all
 
 #Create Posts
 50.times do
     
   Post.create!(
-    topic: topics.sample,
-    title: RandomData.random_sentence,
-    body: RandomData.random_paragraph
+    user:   users.sample,
+    topic:  topics.sample,
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
   )
 end
 
@@ -38,8 +50,14 @@ posts = Post.all
   )
 end
 
+user = User.first
+user.update_attributes!(
+  email: 'kurt.thomas34@gmail.com',
+  password: 'itsatrap'
+)
 
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
