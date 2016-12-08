2129880010d6c91f653805846803077a41b116fe
Kurt Thomas <kurt.thomas34@gmail.com>
2016-12-08 04:46:44 +0000

    assignment 28 complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,8 @@ class PostsController < ApplicationController
   before_action :require_sign_in, except: :show
   
   before_action :authorize_user, except: [:show, :new, :create]
+  before_action :authorize_destroy, only: :destroy
+  
   
   def show
     @post = Post.find(params[:id])
@@ -67,8 +69,17 @@ class PostsController < ApplicationController
   def authorize_user
     post = Post.find(params[:id])
     
-    unless current_user == post.user || current_user.admin?
+    unless current_user == post.user || current_user.admin? || current_user.moderator?
       flash[:alert] = "You must be an admin to do that."
+      redirect_to [post.topic, post]
+    end
+  end
+  
+  def authorize_destroy
+    post = Post.find(params[:id])
+    
+    unless current_user == post.user || current_user.admin?
+      flash[:alert] = "You do not have permission to delete this post."
       redirect_to [post.topic, post]
     end
   end
