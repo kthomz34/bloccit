0e981ee39936cb252dc9515592ff49bbd5ef3f33
Kurt Thomas <kurt.thomas34@gmail.com>
2016-12-15 17:54:09 +0000

    saving assignment 32

--- app/helpers/favorites_helper.rb
+++ app/helpers/favorites_helper.rb
@@ -1,2 +1,9 @@
 module FavoritesHelper
+  def user_has_favorites?
+    if @user.favorites.empty?
+      "#{@user.name} has not favorited any posts yet."
+    else
+      render @user.favorites
+    end
+  end
 end
